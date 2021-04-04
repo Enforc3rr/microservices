feb03c0501eee46d8db0fc779b9e8bc6886cedf9
codernishchay <nishchay.vns2000@gmail.com>
2021-04-04 18:02:38 +0530

    simpleHttpServer.go  added day1

---
+++ ms1/simpleHttpServer.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello world")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+		}
+		log.Printf("Data %s\n", d)
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("goodbye world")
+	})
+	http.ListenAndServe(":9090", nil)
+}
