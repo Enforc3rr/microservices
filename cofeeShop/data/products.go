899445ded08d1b3f76bc8de382c1ddd9d6b24f58
codernishchay <nishchay.vns2000@gmail.com>
2021-04-05 17:21:04 +0530

    product model

---
+++ cofeeShop/data/products.go
@@ -0,0 +1,35 @@
+package data
+
+import "time"
+
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float32
+	SKU         string
+	CreatedON   string
+	UpdatedON   string
+	DeletedON   string
+}
+
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Forthy and milky",
+		Price:       2.34,
+		SKU:         "abc233",
+		CreatedON:   time.Now().UTC().String(),
+		UpdatedON:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Forthy and milky",
+		Price:       2.34,
+		SKU:         "abc233",
+		CreatedON:   time.Now().UTC().String(),
+		UpdatedON:   time.Now().UTC().String(),
+	},
+}
